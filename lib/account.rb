470680fc13d51c2df2491b8fde3079ea6ed96f36
ZishJawaid <zisheanj@gmail.com>
2020-10-07 15:56:10 +0100

    Build: Add test and implementation for logging transactions

--- lib/account.rb
+++ lib/account.rb
@@ -1,28 +1,30 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transaction_log
 
   def initialize(balance = 0)
 
     @balance = balance
+    @transaction_log = []
 
   end
   
-  def deposit(amount)
+  def deposit(amount, date = Time.new.strftime("%d/%m/%Y"))
     raise "Please deposit a positive amount" if amount.negative?
-
     @balance += amount
+    @transaction_log << [date, amount, nil, balance]
   
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date = Time.new.strftime("%d/%m/%Y"))
     raise "Balance has been exceeded. Please withdraw a smaller amount" if balance_exceeded?(amount)
     raise "Please withdraw a positive amount" if amount.negative?
-
     @balance -= amount
+    @transaction_log << [date, nil, amount, balance] 
 
   end
 
+  
   private
 
   def balance_exceeded?(amount)
