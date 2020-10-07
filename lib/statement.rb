fc5f3594a4e35f6e7c03c05e696473bc56b85c1b
ZishJawaid <zisheanj@gmail.com>
2020-10-07 19:02:34 +0100

    Build: statement can be printed from transaction log

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,22 @@
+require_relative 'account.rb'
+
 class Statement
 
-  def print
-    "date || credit || debit || balance"
+  def print(transaction_log = false)
+    
+    if transaction_log
+      array = transaction_log.map { |log| log.join(" || ")}
+      statement = array.reverse.join("\n ")
+      puts statement
+      statement
+    else
+      header
+    end
   end
 
+  def header
+    "date || credit || debit || balance"
+  end
 end
+
+
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,7 +3,7 @@ require 'statement'
 describe Statement do
    
   it 'prints the account statement header' do
-    expect(subject.print).to eq("date || credit || debit || balance")
+    expect(subject.header).to eq("date || credit || debit || balance")
   end
 
   it 'prints out the transaction log' do
@@ -11,6 +11,6 @@ describe Statement do
     account = Account.new
     account.deposit(1000)
     account.withdraw(500)
-    expect(subject.print).to eq("date || credit || debit || balance/n #{date} || 1000 || || 1000/n #{date} || || 500 || 500")
+    expect(subject.print(account.transaction_log)).to eq("#{date} ||  || 500 || 500\n #{date} || 1000 ||  || 1000")
   end
 end
