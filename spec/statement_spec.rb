637205e357340bde38b4a0c90e15adbe5625a7dc
ZishJawaid <zisheanj@gmail.com>
2020-10-07 17:52:46 +0100

    Test: statement can be printed from transaction log

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,16 @@
 require 'statement'
 
 describe Statement do
+   
   it 'prints the account statement header' do
     expect(subject.print).to eq("date || credit || debit || balance")
   end
 
-
+  it 'prints out the transaction log' do
+    date = Time.new.strftime("%d/%m/%Y")
+    account = Account.new
+    account.deposit(1000)
+    account.withdraw(500)
+    expect(subject.print).to eq("date || credit || debit || balance/n #{date} || 1000 || || 1000/n #{date} || || 500 || 500")
+  end
 end
